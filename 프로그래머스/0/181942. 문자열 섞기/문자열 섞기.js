05c20c0badd83ec2889c642944727411286a0ba2
wkdwhdghks <98254580+wkdwhdghks@users.noreply.github.com>
2024-07-01 20:13:34 +0900

    [level 0] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,10 @@
+function solution(str1, str2) {
+    let str = "";
+    
+    for (let i = 0; i < str1.length; i++) {
+        str += str1[i];
+        str += str2[i];
+    }
+    
+    return str
+}

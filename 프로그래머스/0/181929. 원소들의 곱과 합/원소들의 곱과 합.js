dbaa0128c6549cbb427ce8cd5e7f6180c8f2bb11
wkdwhdghks <98254580+wkdwhdghks@users.noreply.github.com>
2024-07-10 20:54:49 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,5 +1,5 @@
 function solution(num_list) {
-    const num1 = num_list.reduce((a, b) => a * b)
-    const num2 = num_list.reduce((a, b) => a + b) ** 2
+    const num1 = num_list.reduce((a, c) => a * c)
+    const num2 = num_list.reduce((a, c) => a + c) ** 2
     return num1 > num2 ? 0 : 1
 }

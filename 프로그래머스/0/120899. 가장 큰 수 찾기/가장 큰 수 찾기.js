c3b1713fc7bef43da813be18cf855e13179f6de0
wkdwhdghks <98254580+wkdwhdghks@users.noreply.github.com>
2024-08-28 20:10:54 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,4 +1,4 @@
 function solution(array) {
-    let num = [...array].sort((a, b) => b - a)[0];
-    return [num, array.indexOf(num)];
+    let max = Math.max(...array);
+    return [max, array.indexOf(max)];
 }

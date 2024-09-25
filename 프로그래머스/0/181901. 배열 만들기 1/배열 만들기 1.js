e2466fb00d0ea5a0e83f3d41c077d3215c0ea609
wkdwhdghks <98254580+wkdwhdghks@users.noreply.github.com>
2024-09-25 18:52:24 +0900

    [level 0] Title: 배열 만들기 1, Time: 9.06 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -0,0 +1,11 @@
+function solution(n, k) {
+    let result = [];
+    
+    for (let i = k; i <= n; i++) {
+        if (i % k === 0) {
+            result.push(i);
+        }
+    }
+    
+    return result;
+}

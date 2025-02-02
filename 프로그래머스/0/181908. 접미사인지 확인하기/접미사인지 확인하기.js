c38bac21fd9edc50407ca530849ddba15ed3f47b
Jonghwan Jang <98254580+wkdwhdghks@users.noreply.github.com>
2025-02-02 20:52:52 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,9 +1,3 @@
 function solution(my_string, is_suffix) {
-    for (let i = 0; i <= my_string.length; i++) {
-        if (my_string.slice(i) === is_suffix) {
-            return 1;
-        }
-    }
-    
-    return 0;
+    return +my_string.endsWith(is_suffix);
 }

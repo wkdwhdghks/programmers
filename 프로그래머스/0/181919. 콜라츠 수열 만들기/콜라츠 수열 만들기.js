5061e6f32befc8f9b896d3e2a4e417942375f369
Jonghwan Jang <98254580+wkdwhdghks@users.noreply.github.com>
2025-02-05 20:34:50 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,12 +1,14 @@
 function solution(n) {
     let arr = [n];
     
-    while (arr[arr.length - 1] !== 1) {
-        if (arr[arr.length - 1] % 2 === 0) {
-            arr.push(arr[arr.length - 1] / 2)
+    while (n !== 1) {
+        if (n % 2 === 0) {
+            n = n / 2;
         } else {
-            arr.push(arr[arr.length - 1] * 3 + 1)
+            n = n * 3 + 1;
         }
+        
+        arr.push(n);
     }
     
     return arr;

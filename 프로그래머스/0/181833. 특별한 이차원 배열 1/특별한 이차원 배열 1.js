2b60d50020811f08f370522563c52d7c7e4b6eb0
Jonghwan Jang <98254580+wkdwhdghks@users.noreply.github.com>
2025-02-10 19:40:46 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.16 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,6 +1,6 @@
 function solution(n) {
     return new Array(n).fill(0).map((el, index) => {
-        const arr = new Array(n).fill(0);
+        let arr = new Array(n).fill(0);
         arr[index] = 1;
         return arr;
     });

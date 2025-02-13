c6494ed0b259db7bae641f08a5e76783a073182e
Jonghwan Jang <98254580+wkdwhdghks@users.noreply.github.com>
2025-02-13 20:09:48 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.07 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,6 +1,6 @@
 function solution(a, b) {
-    let num1 = a % 2 === 0 ? true : false;
-    let num2 = b % 2 === 0 ? true : false;
+    let num1 = a % 2 === 0;
+    let num2 = b % 2 === 0;
     
     if (!num1 && !num2) {
         return Math.pow(a, 2) + Math.pow(b, 2);

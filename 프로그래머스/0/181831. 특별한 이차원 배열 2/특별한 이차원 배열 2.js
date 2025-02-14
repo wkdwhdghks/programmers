af780ec75fc8ea3a02db458d5997cc6aee4ad312
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-02-14 21:52:57 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    let result = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i][j] !== arr[j][i]) {
+                result += 1;
+            }
+        }
+    }
+    
+    return result ? 0 : 1;
+}

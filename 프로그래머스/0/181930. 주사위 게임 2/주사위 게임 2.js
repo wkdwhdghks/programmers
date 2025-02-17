ba76bc33add53a54926237fb2b114cbd3324675b
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-02-17 21:15:20 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,11 +1,15 @@
 function solution(a, b, c) {
     let size = new Set([a, b , c]).size
+    let sum1 = a + b + c;
+    let sum2 = a ** 2 + b ** 2 + c ** 2;
+    let sum3 = a ** 3 + b ** 3 + c ** 3;
     
-    if (size === 3) {
-        return a + b + c;
-    } else if (size === 2) {
-        return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
-    } else {
-        return (a + b + c) * (Math.pow(a, 2) + Math.pow(a, 2) + Math.pow(b, 2)) * (Math.pow(c, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+    switch (size) {
+        case 3:
+            return sum1;
+        case 2:
+            return sum1 * sum2;
+        case 1:
+            return sum1 * sum2 * sum3;
     }
 }

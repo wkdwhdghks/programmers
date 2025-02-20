018194d4bc2e35590cacf76208c6edfc5e2c4f45
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-02-20 22:15:49 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 10.75 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -1,3 +1,3 @@
 function solution(myString) {
-    return myString.split('x').filter(el => el && el).sort((a, b) => a.localeCompare(b));
+    return myString.split('x').filter(el => el !== '').sort();
 }

1ff7308f929ef50b24d884d2ea570eb1a1e93836
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-02-24 21:54:00 +0900

    [level 0] Title: 세로 읽기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -2,7 +2,7 @@ function solution(my_string, m, c) {
     let result = '';
     
     for (let i = 0; i < my_string.length; i += m) {
-        result += [...my_string.slice(i, i + m)][c - 1];
+        result += my_string[i + c - 1];
     }
     
     return result;

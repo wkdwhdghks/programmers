5f5848a409be17645c85291a164f15cc7b82ebc4
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-03-26 23:10:26 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,11 @@
+function solution(myString, pat) {
+    let result = 0;
+    
+    for (let i = 0; i < myString.length; i++) {
+        if (myString.substr(i, pat.length) === pat) {
+            result += 1;
+        };
+    }
+    
+    return result;
+}

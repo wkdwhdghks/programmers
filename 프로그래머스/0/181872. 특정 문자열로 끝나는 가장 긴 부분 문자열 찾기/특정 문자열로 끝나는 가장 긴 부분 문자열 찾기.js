d714768f897ed603cdf554ecec405f1ebd1a5bb0
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-03-27 20:37:17 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
@@ -1,3 +1,3 @@
 function solution(myString, pat) {
-    return myString.substr(0, myString.lastIndexOf(pat) + pat.length)
+    return myString.substring(0, myString.lastIndexOf(pat) + pat.length)
 }

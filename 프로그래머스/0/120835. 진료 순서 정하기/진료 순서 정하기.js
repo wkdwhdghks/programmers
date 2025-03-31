3c3d3c59527ddd5c5b5c1f1ad750027a8191c1d8
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-03-31 17:06:20 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,4 @@
 function solution(emergency) {
-    let arr = [...emergency].sort((a, b) => b - a);
+    const arr = [...emergency].sort((a, b) => b - a);
     return emergency.map(el => arr.indexOf(el) + 1);
 }

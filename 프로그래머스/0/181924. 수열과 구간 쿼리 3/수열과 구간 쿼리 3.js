64d51d50c9daa8d3b872d66a7b38b7f100f278f7
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-05-15 22:56:53 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.55 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,7 +1,7 @@
 function solution(arr, queries) {
-    for (const [i, j] of queries) {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
+    queries.forEach( ([a,b]) => {
+        [arr[a],arr[b]] = [arr[b],arr[a]];
+    })
     
     return arr;
 }

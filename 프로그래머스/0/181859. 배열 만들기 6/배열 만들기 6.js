2d268a78ac149055052416e1f02b56584a348e03
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-05-22 20:10:14 +0900

    [level 0] Title: 배열 만들기 6, Time: 33.63 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    let result = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (result.length === 0) {
+            result.push(arr[i]);
+        } else if (result.length !== 0 && result[result.length - 1] === arr[i]) {
+            result.pop();
+        } else if (result.length !== 0 && result[result.length - 1] !== arr[i]) {
+            result.push(arr[i]);
+        }
+    }
+    
+    return result.length > 0 ? result : [-1];
+}

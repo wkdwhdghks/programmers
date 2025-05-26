abf99bba93ed83df58bcab2557da171ad9af40da
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-05-26 20:59:31 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,14 +1,15 @@
 function solution(my_string) {
-    const counts = new Array(52).fill(0);
+    const result = new Array(52).fill(0);
     
-    for (const ch of my_string) {
-        const code = ch.charCodeAt(0);
+    for (const i of my_string) {
+        const code = i.charCodeAt(0);
+        
         if (code >= 65 && code <= 90) {
-            counts[code - 65]++;
+            result[code - 65]++;
         } else if (code >= 97 && code <= 122) {
-            counts[code - 97 + 26]++;
+            result[code - 97 + 26]++;
         }
     }
     
-    return counts;
+    return result;
 }

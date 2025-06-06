b4ad8bb562a1718a893c3df0d306dc1e1da00d94
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-06-06 23:42:24 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2.92 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,18 +1,3 @@
 function solution(arr, k) {
-    const result = [];
-    const seen = new Set();
-    
-    for (const num of arr) {
-        if (!seen.has(num)) {
-            seen.add(num);
-            result.push(num);
-            if (result.length === k) break;
-        }
-    }
-    
-    while (result.length < k) {
-        result.push(-1);
-    }
-    
-    return result;
+    return [...Array.from(new Set(arr)), ...new Array(k).fill(-1)].slice(0, k);
 }

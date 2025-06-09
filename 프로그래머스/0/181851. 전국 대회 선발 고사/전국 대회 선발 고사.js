9a01777ded3a4509b17e9d1cee3152ff7fe0e466
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-06-09 21:06:55 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,8 +1,5 @@
 function solution(rank, attendance) {
-    const filtered = rank.map((el, index) => [index, el]).filter(([i, _]) => attendance[i]);
-    filtered.sort((a, b) => a[1] - b[1]);
-    
-    const [a, b, c] = filtered.slice(0, 3).map(([i, _]) => i);
-    return 10000 * a + 100 * b + c;
+    const [a, b, c] = rank.map((r, i) => [r, i]).filter(([_, i]) => attendance[i]).sort(([a], [b]) => a - b);
+    return 10000 * a[1] + 100 * b[1] + c[1];
 }
 

8931bf21c2727ca010a14597b89f843dcb8534ec
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-06-25 20:23:54 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 66.30 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -9,5 +9,5 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    console.log(`${input[0]}${input[1]}`);
+    console.log(input.join(''));
 });

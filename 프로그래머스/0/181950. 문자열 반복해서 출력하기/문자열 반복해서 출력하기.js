db96bc329c8b18fb5404de774244cea1de297a04
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-06-27 23:01:11 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 46.13 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    let result = '';
+    
+    for (let i = 0; i < input[1]; i++) {
+        result += input[0];
+    }
+    
+    console.log(result);
+});

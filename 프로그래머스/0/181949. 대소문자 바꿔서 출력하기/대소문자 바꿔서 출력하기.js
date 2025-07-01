4586e2e180230dfbc7ec059d84835fa193a497d5
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-07-01 20:18:09 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 64.89 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    let result = '';
+    
+    for (let i of str) {
+        if (i === i.toUpperCase()) {
+            result += i.toLowerCase();
+        } else {
+            result += i.toUpperCase();
+        }
+    }
+    
+    console.log(result);
+});

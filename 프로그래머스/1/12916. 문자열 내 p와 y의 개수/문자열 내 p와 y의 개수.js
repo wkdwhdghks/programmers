458e2b587f4fd491b2be2c2dc28e0e350e372d12
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-07-15 20:34:01 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,7 +1,3 @@
 function solution(s){
-    const str = s.toLowerCase();
-    const p = str.split('p').length - 1;
-    const y = str.split('y').length - 1;
-    
-    return p === y || !p && !y ? true : false;
+    return s.match(/p/ig)?.length === s.match(/y/ig)?.length;
 }

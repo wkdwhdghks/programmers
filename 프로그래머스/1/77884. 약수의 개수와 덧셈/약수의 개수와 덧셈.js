7ab7c51900975b67dfaa37ddce342b4edd338f24
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-08-06 20:17:46 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,13 @@
+function solution(left, right) {
+    let result = 0;
+    
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            result -= i;
+        } else {
+            result += i;
+        }
+    }
+    
+    return result;
+}

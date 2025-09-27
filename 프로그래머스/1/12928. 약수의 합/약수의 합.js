a34a59af6104b12cb1d9291a5455b62c9b16c344
장종환 Jonghwan Jang <wkdwhdghks97@gmail.com>
2025-09-27 10:23:10 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -2,9 +2,7 @@ function solution(n) {
     let result = 0;
     
     for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            result += i;
-        }
+        if (n % i === 0) result += i;
     }
     
     return result;
